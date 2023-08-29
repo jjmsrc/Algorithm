3d29c6890e1ac354af0647a26284d12387f3a51a
jjmsrc <140387465+jjmsrc@users.noreply.github.com>
2023-08-29 15:59:31 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18056 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -11,11 +11,12 @@ public class Main {
 		mem[1] = 0;
 		
 		for (int i = 2; i <= X; i++) {
-			mem[i] = mem[i - 1] + 1;
-			if (i % 2 == 0 && mem[i] > mem[i / 2] + 1)
-				mem[i] = mem[i / 2] + 1;
-			if (i % 3 == 0 && mem[i] > mem[i / 3] + 1)
-				mem[i] = mem[i / 3] + 1;
+			mem[i] = mem[i - 1];
+			if (i % 2 == 0 && mem[i] > mem[i / 2])
+				mem[i] = mem[i / 2];
+			if (i % 3 == 0 && mem[i] > mem[i / 3])
+				mem[i] = mem[i / 3];
+			mem[i]++;
 		}
 
 		System.out.println(mem[X]);
