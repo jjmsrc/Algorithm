cecb8b3e17e18db5421fc86941bca0d261543bcc
jjmsrc <140387465+jjmsrc@users.noreply.github.com>
2023-08-30 15:15:54 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 896 ms, Memory: 69584 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,6 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+/*
+ * 1. 문제 해석
+ * 		말이 되고픈 원숭이가 움직이면서 도착지점까지 갈 때 그 동작수의 최솟값을 출력한다.
+ * 		원숭이는 말처럼 K번만 움직일 수 있으며, 상하좌우로 움직일 수 있다.
+ * 		말처럼 움직일 때는 벽을 넘을 수 있다.
+ * 		격자판의 맨 왼쪽 위에서 시작해서 맨 오른쪽 아래까지 가야한다.
+ * 
+ * 2. 해결 전략
+ * 		W*H*(K+1) 의 메모리 배열을 만들어 DP를 수행한다.
+ * 
+ * 3. 주의점
+ * 
+ * */
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
@@ -22,19 +36,19 @@ public class Main {
 			}
 		}
 		
-		int[] dxsHorse = {-1, -2, -2, -1, 1, 2, 2, 1}; // 반시계방향으로
+		int[] dxsHorse = {-1, -2, -2, -1, 1, 2, 2, 1}; // 오른쪽에서 반시계방향으로
 		int[] dysHorse = {2, 1, -1, -2, -2, -1, 1, 2};
 		int[] dxs = {1, -1, 0, 0};
 		int[] dys = {0, 0, 1, -1};
 		
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+		Queue<int[]> q = new ArrayDeque<int[]>();
 		
 		int[][][] mem = new int[W][H][K + 1];
 		mem[0][0][K] = 1;
-		pq.offer(new int[] {1, 0, 0, K});
-		while(!pq.isEmpty()) {
+		q.offer(new int[] {1, 0, 0, K});
+		while(!q.isEmpty()) {
 			
-			int[] d = pq.poll();
+			int[] d = q.poll();
 			int r = d[1];
 			int c = d[2];
 			int k = d[3];
@@ -50,7 +64,7 @@ public class Main {
 					if (nx >= 0 && nx < W && ny >= 0 && ny < H && map[nx][ny] == 0) {
 						if (mem[nx][ny][k - 1] == 0 || mem[nx][ny][k - 1] > mem[r][c][k] + 1) {
 							mem[nx][ny][k - 1] = mem[r][c][k] + 1;
-							pq.offer(new int[] {mem[nx][ny][k - 1], nx, ny, k - 1});
+							q.offer(new int[] {mem[nx][ny][k - 1], nx, ny, k - 1});
 						}
 					}
 				}
@@ -60,11 +74,12 @@ public class Main {
 				if (nx >= 0 && nx < W && ny >= 0 && ny < H && map[nx][ny] == 0) {
 					if (mem[nx][ny][k] == 0 || mem[nx][ny][k] > mem[r][c][k] + 1) {
 						mem[nx][ny][k] = mem[r][c][k] + 1;
-						pq.offer(new int[] {mem[nx][ny][k], nx, ny, k});
+						q.offer(new int[] {mem[nx][ny][k], nx, ny, k});
 					}
 				}
 			}
 		}
+		
 		int min = Integer.MAX_VALUE;
 		for (int i = 0; i <= K; i++) {
 			if (mem[W - 1][H - 1][i] > 0 && min > mem[W - 1][H - 1][i])
