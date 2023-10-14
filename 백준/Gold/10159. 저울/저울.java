21746a469ea9ed9a103d58c66f55842c945901de
jjmsrc <140387465+jjmsrc@users.noreply.github.com>
2023-10-14 12:27:04 +0900

    [Gold IV] Title: 저울, Time: 164 ms, Memory: 15124 KB -BaekjoonHub

--- 백준/Gold/10159. 저울/저울.java
+++ 백준/Gold/10159. 저울/저울.java
@@ -13,35 +13,36 @@ public class Main {
 		int M = Integer.parseInt(br.readLine());
 
 		int[][] adjMat = new int[N + 1][N + 1];
-		int[][] adjMatR = new int[N + 1][N + 1];
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			adjMat[u][v] = 1;
-			adjMatR[v][u] = 1;
 		}
 
 		for (int i = 1; i <= N; i++) {
 			adjMat[i][0] = -1;
-			adjMatR[i][0] = -1;
 		}
 
 		for (int i = 1; i <= N; i++) {
-			dfs(adjMat, i);
-			dfs(adjMatR, i);
+			gtdfs(adjMat, i);
 		}
-
+		
 		for (int i = 1; i <= N; i++) {
-			sb.append(N - adjMat[i][0] - adjMatR[i][0] - 1).append("\n");
+			int cntlt = 0;
+			for (int j = 1; j <= N; j++) {
+				if (adjMat[j][i] == 1)
+					cntlt++;
+			}
+			sb.append(N - 1 - adjMat[i][0] - cntlt).append("\n");
 		}
 
 		System.out.println(sb);
 
 	}
 
-	private static void dfs(int[][] adjMat, int curr) {
+	private static void gtdfs(int[][] adjMat, int curr) {
 		if (adjMat[curr][0] != -1)
 			return;
 
@@ -49,7 +50,7 @@ public class Main {
 			if (adjMat[curr][i] == 0)
 				continue;
 			if (adjMat[i][0] == -1)
-				dfs(adjMat, i);
+				gtdfs(adjMat, i);
 			for (int j = 1; j < adjMat.length; j++) {
 				if (adjMat[i][j] == 1)
 					adjMat[curr][j] = 1;
