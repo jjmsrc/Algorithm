dce7c676cb79a88bc852c059aa1015c7a3225876
김지민 <140387465+jjmsrc@users.noreply.github.com>
2025-09-16 02:47:19 +0900

    [Gold V] Title: AC, Time: 88 ms, Memory: 39132 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,38 +1,41 @@
+import sys
+
+inp = sys.stdin.readline
+
+
 def solve():
-    p = input()
-    n = int(input())
-    arr = input()[1:-1].split(',')
+    cmd = inp().rstrip()
+    n = int(inp())
+    arr = inp()[1:-2].split(',')
+
     if len(arr[0]) == 0:
         arr = []
 
+    is_reversed = False
     start = 0
     end = 0
-    is_reversed = False
-    for f in p:
-        if f == 'D':
+
+    for c in cmd:
+        if c == 'R':
+            is_reversed = not is_reversed
+        if c == 'D':
             if is_reversed:
                 end += 1
             else:
                 start += 1
-        else:
-            is_reversed = not is_reversed
 
-    if start + end > n:
+    if start + end > len(arr):
         print('error')
-    else:
-        if len(arr) == 0:
-            print("[]")
-        else:
-            if end == 0:
-                arr = arr[start:]
-            else:
-                arr = arr[start:-end]
-            if is_reversed:
-                arr = arr[::-1]
-            print("[%s]" % ",".join(arr))
+        return
+
+    arr = arr[start:len(arr) - end]
+    if is_reversed:
+        arr = arr[::-1]
+
+    print(f"[{','.join(arr)}]")
 
 
 if __name__ == '__main__':
-    T = int(input())
-    for _ in range(T):
+    T = int(inp())
+    for i in range(T):
         solve()
